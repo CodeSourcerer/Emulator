1f16a5752e1c8b8a79aab610a82ab416cbdd8055
cstrawinsk <codesourcerer@gmail.com>
2019-09-27 08:50:55 -0500

    Somehow missed adding the BusDevice interface

---
+++ CS6502/BusDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CS6502
+{
+    public interface BusDevice
+    {
+        BusDeviceType DeviceType { get; }
+
+        bool Write(ushort addr, byte data);
+        bool Read(ushort addr, out byte data);
+        void Reset();
+    }
+}
