85c6ee8f937263ca3d967a58ee84df66b43e51a3
cstrawinsk <codesourcerer@gmail.com>
2019-10-22 08:59:58 -0500

    Make lookup table static, because I just realized every channel will need to have their own LengthCounter but we don't need to duplicate that table.

--- NESEmulator/APULengthCounter.cs
+++ NESEmulator/APULengthCounter.cs
@@ -10,11 +10,15 @@ namespace NESEmulator
 
         public event EventHandler CounterElapsed;
 
-        private Dictionary<byte, byte> _lengthTable; // { get; set; }
+        private static Dictionary<byte, byte> _lengthTable; // { get; set; }
+
+        static APULengthCounter()
+        {
+            buildLengthTable();
+        }
 
         public APULengthCounter()
         {
-            buildLengthTable();
         }
 
         public void Clock()
@@ -39,41 +43,43 @@ namespace NESEmulator
             LinearLength = 0;
         }
 
-        private void buildLengthTable()
+        private static void buildLengthTable()
         {
-            _lengthTable = new Dictionary<byte, byte>();
-            _lengthTable.Add(0x00, 10);
-            _lengthTable.Add(0x01, 254);
-            _lengthTable.Add(0x02, 20);
-            _lengthTable.Add(0x03, 2);
-            _lengthTable.Add(0x04, 40);
-            _lengthTable.Add(0x05, 4);
-            _lengthTable.Add(0x06, 80);
-            _lengthTable.Add(0x07, 6);
-            _lengthTable.Add(0x08, 160);
-            _lengthTable.Add(0x09, 8);
-            _lengthTable.Add(0x0A, 60);
-            _lengthTable.Add(0x0B, 10);
-            _lengthTable.Add(0x0C, 14);
-            _lengthTable.Add(0x0D, 12);
-            _lengthTable.Add(0x0E, 26);
-            _lengthTable.Add(0x0F, 14);
-            _lengthTable.Add(0x10, 12);
-            _lengthTable.Add(0x11, 16);
-            _lengthTable.Add(0x12, 24);
-            _lengthTable.Add(0x13, 18);
-            _lengthTable.Add(0x14, 48);
-            _lengthTable.Add(0x15, 20);
-            _lengthTable.Add(0x16, 96);
-            _lengthTable.Add(0x17, 22);
-            _lengthTable.Add(0x18, 192);
-            _lengthTable.Add(0x19, 24);
-            _lengthTable.Add(0x1A, 72);
-            _lengthTable.Add(0x1B, 26);
-            _lengthTable.Add(0x1C, 16);
-            _lengthTable.Add(0x1D, 28);
-            _lengthTable.Add(0x1E, 32);
-            _lengthTable.Add(0x1F, 30);
+            _lengthTable = new Dictionary<byte, byte>
+            {
+                { 0x00, 10  },
+                { 0x01, 254 },
+                { 0x02, 20  },
+                { 0x03, 2   },
+                { 0x04, 40  },
+                { 0x05, 4   },
+                { 0x06, 80  },
+                { 0x07, 6   },
+                { 0x08, 160 },
+                { 0x09, 8   },
+                { 0x0A, 60  },
+                { 0x0B, 10  },
+                { 0x0C, 14  },
+                { 0x0D, 12  },
+                { 0x0E, 26  },
+                { 0x0F, 14  },
+                { 0x10, 12  },
+                { 0x11, 16  },
+                { 0x12, 24  },
+                { 0x13, 18  },
+                { 0x14, 48  },
+                { 0x15, 20  },
+                { 0x16, 96  },
+                { 0x17, 22  },
+                { 0x18, 192 },
+                { 0x19, 24  },
+                { 0x1A, 72  },
+                { 0x1B, 26  },
+                { 0x1C, 16  },
+                { 0x1D, 28  },
+                { 0x1E, 32  },
+                { 0x1F, 30  }
+            };
         }
     }
 }
