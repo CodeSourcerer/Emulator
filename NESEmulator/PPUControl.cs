b2c118a3503ef90eb5c2eb9caf507ad3cf784bfa
Chad Strawinski <codesourcerer@gmail.com>
2019-12-27 22:53:37 -0600

    Clean up PPUControl reg struct

--- NESEmulator/PPUControl.cs
+++ NESEmulator/PPUControl.cs
@@ -9,98 +9,59 @@ namespace NESEmulator
 
         public bool NameTableX
         {
-            get
-            {
-                return reg.TestBit(0);
-            }
-            set
-            {
-                reg = reg.SetBit(0, value);
-            }
+            get => reg.TestBit(0);
+            set => reg = reg.SetBit(0, value);
         }
 
         public bool NameTableY
         {
-            get
-            {
-                return reg.TestBit(1);
-            }
-            set
-            {
-                reg = reg.SetBit(1, value);
-            }
+            get => reg.TestBit(1);
+            set => reg = reg.SetBit(1, value);
         }
 
         public bool IncrementMode
         {
-            get
-            {
-                return reg.TestBit(2);
-            }
-            set
-            {
-                reg = reg.SetBit(2, value);
-            }
+            get => reg.TestBit(2);
+            set => reg = reg.SetBit(2, value);
         }
 
+        /// <summary>
+        /// Indicates which Sprite Pattern table to use (0x0000 : false, 0x1000 : true)
+        /// </summary>
         public bool PatternSprite
         {
-            get
-            {
-                return reg.TestBit(3);
-            }
-            set
-            {
-                reg = reg.SetBit(3, value);
-            }
+            get => reg.TestBit(3);
+            set => reg = reg.SetBit(3, value);
         }
 
+        /// <summary>
+        /// Indicates which Background Pattern table to use (0x0000 : false, 0x1000 : true)
+        /// </summary>
         public bool PatternBackground
         {
-            get
-            {
-                return reg.TestBit(4);
-            }
-            set
-            {
-                reg = reg.SetBit(4, value);
-            }
+            get => reg.TestBit(4);
+            set => reg = reg.SetBit(4, value);
         }
 
+        /// <summary>
+        /// Indicates 8x8 or 8x16 sprite. 8x8 : false, 8x16 : true
+        /// </summary>
         public bool SpriteSize
         {
-            get
-            {
-                return reg.TestBit(5);
-            }
-            set
-            {
-                reg = reg.SetBit(5, value);
-            }
+            get => reg.TestBit(5);
+            set => reg = reg.SetBit(5, value);
         }
 
         public bool SlaveMode
         {
-            get
-            {
-                return reg.TestBit(6);
-            }
-            set
-            {
-                reg = reg.SetBit(6, value);
-            }
+            get => reg.TestBit(6);
+            set => reg = reg.SetBit(6, value);
         }
 
         public bool EnableNMI
         {
-            get
-            {
-                return reg.TestBit(7);
-            }
-            set
-            {
-                reg = reg.SetBit(7, value);
-            }
+            get => reg.TestBit(7);
+            set => reg = reg.SetBit(7, value);
         }
     }
 }
